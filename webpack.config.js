1d83a350effee178d0356749730240d922fcdd95
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2023-08-22 15:11:21 +0200

    Add ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,22 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    mode: "production",
-    entry: {
-        index: path.resolve(__dirname, './js/index.js')
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname),
-    },
-    target: ['web', 'es5'],
-    plugins: [
-        new NodePolyfillPlugin(),
-        new webpack.DefinePlugin({
-            CLIENT_ID: JSON.stringify("https://solid-md-viewer.netlify.app/id")
-        })
-    ],
-    watchOptions: {
-        ignored: ['**/node_modules', '**/.idea'],
-    }
+  mode: "production",
+  entry: {
+    index: path.resolve(__dirname, './js/index.js')
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname),
+  },
+  target: ['web', 'es5'],
+  plugins: [
+    new NodePolyfillPlugin(),
+    new webpack.DefinePlugin({
+      CLIENT_ID: JSON.stringify("https://solid-md-viewer.netlify.app/id")
+    })
+  ],
+  watchOptions: {
+    ignored: ['**/node_modules', '**/.idea'],
+  }
 };
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,21 +3,21 @@ const webpack = require('webpack');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = Object.assign(config, {
-    mode: "development",
-    devtool: 'source-map',
-    devServer: {
-        headers: (data) => {
-            if (data.baseUrl === '/id') {
-                return {'Content-Type': 'application/ld+json'}
-            }
+  mode: "development",
+  devtool: 'source-map',
+  devServer: {
+    headers: (data) => {
+      if (data.baseUrl === '/id') {
+        return {'Content-Type': 'application/ld+json'}
+      }
 
-            return {};
-        }
-    },
-    plugins: [
-        new NodePolyfillPlugin(),
-        new webpack.DefinePlugin({
-            CLIENT_ID: JSON.stringify("http://localhost:8080/id")
-        })
-    ]
+      return {};
+    }
+  },
+  plugins: [
+    new NodePolyfillPlugin(),
+    new webpack.DefinePlugin({
+      CLIENT_ID: JSON.stringify("http://localhost:8080/id")
+    })
+  ]
 })
